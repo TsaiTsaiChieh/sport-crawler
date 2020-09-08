5104c2f2c7bb65fbf36f977f68b407d79906844e
d0177381@gmail.com <>
2020-09-08 17:08:52 +0800

    initialization

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const Have = require('domain-haven');
+const schedule = require('node-schedule');
+const app = express();
+app.use(Have.haven());
+
+schedule.scheduleJob('*/10 * * * *', function() {
+  console.log('每十秒！！就會有好事發生');
+});
+
+const { PORT } = process.env;
+app.listen(PORT, function() {
+  console.log(`Crawler on port: ${PORT}`);
+});
