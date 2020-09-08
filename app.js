3a84c7aaa5e0e0d20396a4e3720afca8b90d64f1
d0177381@gmail.com <>
2020-09-08 17:17:31 +0800

    [Change] to each 10 second

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const schedule = require('node-schedule');
 const app = express();
 app.use(Have.haven());
 
-schedule.scheduleJob('*/10 * * * *', function() {
+schedule.scheduleJob('*/10 * * * * *', function() {
   console.log('每十秒！！就會有好事發生');
 });
 
