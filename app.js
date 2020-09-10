2679a49a742aa7ae43ecac2d5ac9e18eb487016d
page40316 <page@gets-info.com>
2020-09-10 12:30:42 +0800

    merge master

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const Have = require('domain-haven');
 const schedule = require('node-schedule');
-<<<<<<< HEAD
 const HW = require('./src/handicap/HW/HW_handicap');
-=======
 // const NBA_prematch = require('./src/invoke/baseball/NBA_match');
->>>>>>> master
 // const connection = require('./src/helpers/connection');
 // const mysql = require('./src/helpers/mysqlUtil');
 
@@ -14,7 +11,6 @@ const app = express();
 
 app.use(Have.haven());
 
-<<<<<<< HEAD
 schedule.scheduleJob('0 0 11 * * *', async function() {
   // 取得 Token
   // HW.getToken();
@@ -28,7 +24,6 @@ schedule.scheduleJob('*/3 * * * * *', async function() {
 schedule.scheduleJob('*/3 * * * * *', async function() {
   // 其他測試
   // HW.test();
-=======
 schedule.scheduleJob('*/3 * * * * *', async function() {
   try {
     // console.log('Each 3 second...');
@@ -38,7 +33,6 @@ schedule.scheduleJob('*/3 * * * * *', async function() {
   }
 
   // await connection();
->>>>>>> master
 });
 
 const { PORT } = process.env;
