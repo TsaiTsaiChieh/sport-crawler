8f59c21e6cf84168741588584a3951ea7902f28a
d0177381@gmail.com <>
2020-09-10 12:32:49 +0800

    [Initial]

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const Have = require('domain-haven');
 const schedule = require('node-schedule');
-// const NBA_prematch = require('./src/invoke/baseball/NBA_match');
+const NBA_match = require('./src/invoke/baseball/NBA_match');
+// const NBA_livescore = require('./src/invoke/baseball/NBA_livescore');
 // const connection = require('./src/helpers/connection');
 // const mysql = require('./src/helpers/mysqlUtil');
 
@@ -10,10 +11,20 @@ const app = express();
 
 app.use(Have.haven());
 
-schedule.scheduleJob('*/3 * * * * *', async function() {
+schedule.scheduleJob('*/3 * * * * *', async function(fireDate) {
   try {
-    // console.log('Each 3 second...');
-    // await NBA_prematch();
+    // console.log(`This job was supposed to run at ${fireDate}`);
+    // await NBA_livescore();
+  } catch (err) {
+    console.log(err);
+  }
+  // await connection();
+});
+
+schedule.scheduleJob('0 21 * * *', async function(fireDate) {
+  try {
+    console.log(`This job was supposed to run at ${fireDate} , but actually ran at ${new Date()}`);
+    await NBA_match();
   } catch (err) {
     console.log(err);
   }
---
+++ src/configs/envValues.js
@@ -0,0 +1,10 @@
+exports.firebaseConfig = {
+  apiKey: process.env.firebaseApiKey,
+  authDomain: process.env.firebaseAuthDomain,
+  databaseURL: process.env.firebaseDatabaseURL,
+  projectId: process.env.GCLOUD_PROJECT,
+  storageBucket: process.env.firebaseStorageBucket,
+  messagingSenderId: process.env.firebaseMessagingSenderId,
+  appId: process.env.firebaseAppId,
+  measurementId: process.env.firebaseMeasurementId
+};
