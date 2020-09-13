23c588881925c5137c646718cb850ce4d1f74502
d0177381@gmail.com <>
2020-09-13 10:01:58 +0800

    [Add] teamStatData updating in updateliveScore function

--- src/invoke/baseball/NBA_livescore.js
+++ src/invoke/baseball/NBA_livescore.js
@@ -31,7 +31,7 @@ async function main() {
     const matchData = await getMatchDataFromMySQL(nowUnix);
     await updateMatchInplayStatus2MySQL(matchData);
     await liveTextStart(matchData);
-    await teamStatData(matchData);
+    // await teamStatData(matchData);
   } catch (err) {
     console.log(err);
     return err;
@@ -78,13 +78,14 @@ async function liveTextStart(data) {
   try {
     if (data.length) {
       data.map(async function(ele) {
-        const { league, sport, liveAPI, locale } = configs;
+        const { league, sport, liveAPI, teamComparisonAPI, locale } = configs;
         const gameId = ele.matchId;
-        const URL = `${liveAPI}?gameId=${gameId}&locale=${locale}`;
-        const data = await getData(URL);
-        const path = `${sport}/${league}/${gameId}`;
-        const matchChunk = await repackageMatches(data, gameId, path);
-        await set2realtime(path, matchChunk);
+        const liveURL = `${liveAPI}?gameId=${gameId}&locale=${locale}`;
+        const teamComparisonURL = `${teamComparisonAPI}?gameId=${gameId}`;
+        const liveData = await getData(liveURL);
+        const teamStatData = await getData(teamComparisonURL);
+        const path = `${sport}/${league}/${gameId}/Summary`;
+        await updateLiveAndTeamData({ liveData, teamStatData }, gameId, path);
       });
     }
     return Promise.resolve();
@@ -92,61 +93,58 @@ async function liveTextStart(data) {
     return Promise.reject(err.stack);
   }
 }
+
 // 全部更新
-async function repackageMatches(matchData, gameId, realtimePath) {
+async function updateLiveAndTeamData(matchData, gameId, path) {
   try {
-    const { payload } = matchData;
+    const { liveData, teamStatData } = matchData;
+
+    const { payload } = liveData;
     let playByPlays = [];
     if (payload) playByPlays = payload.playByPlays;
 
     if (playByPlays.length) {
+      await updateTeamsStat(teamStatData, path);
       const { status } = payload.boxscore;
-      await updateMatchEndStatus2DB(status, gameId, realtimePath);
+      await updateMatchEndStatus2DB(status, gameId, path);
       const nowPeriod = payload.boxscore.period;
       const clock = !payload.boxscore.periodClock ? '00:00' : payload.boxscore.periodClock;
       const eventOrderAtNowPeriod = payload.playByPlays[0].events.length;
-      const Summary = {
-        Now_clock: clock,
-        Now_periods: nowPeriod,
-        Now_event_order: eventOrderAtNowPeriod,
-        status: matchStatus[payload.boxscore.status],
-        info: {
-          away: { Total: { points: payload.boxscore.awayScore } },
-          home: { Total: { points: payload.boxscore.homeScore } }
-        }
-      };
-
+      const statusDes = matchStatus[payload.boxscore.status];
+      const awayTotalPoints = payload.boxscore.awayScore;
+      const homeTotalPoints = payload.boxscore.homeScore;
+      await set2realtime(`${path}/Now_periods`, nowPeriod);
+      await set2realtime(`${path}/Now_clock`, clock);
+      await set2realtime(`${path}/Now_event_order`, eventOrderAtNowPeriod);
+      await set2realtime(`${path}/status`, statusDes);
+      await set2realtime(`${path}/info/away/Total/points`, awayTotalPoints);
+      await set2realtime(`${path}/info/home/Total/points`, homeTotalPoints);
       const awayId = payload.gameProfile.awayTeamId;
       const homeId = payload.gameProfile.homeTeamId;
 
       for (let i = 0; i < playByPlays.length; i++) {
         const period = playByPlays[i].period;
-        Summary.info.away[`periods${period}`] = {};
-        Summary.info.home[`periods${period}`] = {};
-        Summary[`periods${period}`] = {};
         // Subtract the points from the previous round
         const awayScoreTmp = parseInt(playByPlays[i].events[playByPlays[i].events.length - 1].awayScore);
         const homeScoreTmp = parseInt(playByPlays[i].events[playByPlays[i].events.length - 1].homeScore);
-
+        const awayScore = parseInt(playByPlays[i].events[0].awayScore) - awayScoreTmp;
+        const homeScore = parseInt(playByPlays[i].events[0].homeScore) - homeScoreTmp;
+        await set2realtime(`${path}/info/away/periods${period}/points`, String(awayScore));
+        await set2realtime(`${path}/info/home/periods${period}/points`, String(homeScore));
         for (let j = playByPlays[i].events.length; j > 0; j--) {
           const event = playByPlays[i].events[j - 1];
-          const awayScore = parseInt(event.awayScore) - awayScoreTmp;
-          const homeScore = parseInt(event.homeScore) - homeScoreTmp;
-          Summary.info.away[`periods${period}`].points = String(awayScore);
-          Summary.info.home[`periods${period}`].points = String(homeScore);
           let descriptionCh = filterSymbol(event.description, ']');
           if (event.messageType === '12') descriptionCh = replaceDescription(period, '開始');
           if (event.messageType === '13') descriptionCh = replaceDescription(period, '結束');
-          Summary[`periods${period}`][`events${playByPlays[i].events.length - j + 1}`] = {
-            Period: period,
-            Clock: event.gameClock,
-            attribution: messageTypeMapping(event.teamId, awayId, homeId),
-            description_ch: descriptionCh
-          };
+          const specificPath = `${path}/periods${period}/events${playByPlays[i].events.length - j + 1}`;
+          await set2realtime(`${specificPath}/Period`, period);
+          await set2realtime(`${specificPath}/Clock`, event.gameClock);
+          await set2realtime(`${specificPath}/attribution`, messageTypeMapping(event.teamId, awayId, homeId));
+          await set2realtime(`${specificPath}/description_ch`, descriptionCh);
         }
       }
-      return Promise.resolve({ Summary });
     }
+
     return Promise.resolve();
   } catch (err) {
     return Promise.reject(new ServerErrors.RepackageError(err.stack));
@@ -182,25 +180,7 @@ function replaceDescription(period, append) {
   return `第${period}節${append}`;
 }
 
-async function teamStatData(data) {
-  try {
-    if (data.length) {
-      data.map(async function(ele) {
-        const { league, sport, teamComparisonAPI } = configs;
-        const gameId = ele.matchId;
-        const URL = `${teamComparisonAPI}?gameId=${gameId}`;
-        const data = await getData(URL);
-        const path = `${sport}/${league}/${gameId}`;
-        await repackageTeamsStat(data, gameId, path);
-      });
-    }
-    return Promise.resolve();
-  } catch (err) {
-    return Promise.reject(err.stack);
-  }
-}
-
-async function repackageTeamsStat(data, gameId, path) {
+async function updateTeamsStat(data, path) {
   try {
     const { payload } = data;
     if (payload) {
@@ -208,19 +188,19 @@ async function repackageTeamsStat(data, gameId, path) {
       const homeStats = payload.homeTeam.score;
       const awayData = {
         fouls: awayStats.fouls,
-        ft_point_attempts: awayStats.fta,
+        ft_point_attempts: awayStats.fta, // 罰球
         ft_point_scored: awayStats.ftm,
         ft_point_percent: awayStats.ftpct,
-        two_point_attempts: awayStats.fga,
-        two_point_scored: awayStats.fgm,
-        two_point_percent: awayStats.fgpct,
+        fg_point_attempts: awayStats.fga, // 投籃
+        fg_point_scored: awayStats.fgm,
+        fg_point_percent: awayStats.fgpct,
         three_point_attempts: awayStats.tpa,
         three_point_scored: awayStats.tpm,
         three_point_percent: awayStats.tppct,
         rebounds: awayStats.rebs, // 籃板 REB
         turnovers: awayStats.turnovers, // 失誤 TO
         assists: awayStats.assists, // 助攻 AST
-        blocks_against: awayStats.blocksAgainst, // 阻攻 BLK
+        blocks: awayStats.blocksAgainst, // 阻攻 BLK
         steals: awayStats.steals, // 抄截 STL
         fast_break_points: awayStats.fastBreakPoints, // 快攻得分 FBP
         points_in_paint: awayStats.pointsInPaint, // 禁區得分 PITP
@@ -232,25 +212,25 @@ async function repackageTeamsStat(data, gameId, path) {
         ft_point_attempts: homeStats.fta,
         ft_point_scored: homeStats.ftm,
         ft_point_percent: homeStats.ftpct,
-        two_point_attempts: homeStats.fga,
-        two_point_scored: homeStats.fgm,
-        two_point_percent: homeStats.fgpct,
+        fg_point_attempts: homeStats.fga,
+        fg_point_scored: homeStats.fgm,
+        fg_percent: homeStats.fgpct,
         three_point_attempts: homeStats.tpa,
         three_point_scored: homeStats.tpm,
         three_point_percent: homeStats.tppct,
-        rebounds: homeStats.rebs, // 籃板 REB
-        turnovers: homeStats.turnovers, // 失誤 TO
-        assists: homeStats.assists, // 助攻 AST
-        blocks_against: homeStats.blocksAgainst, // 阻攻 BLK
-        steals: homeStats.steals, // 抄截 STL
-        fast_break_points: homeStats.fastBreakPoints, // 快攻得分 FBP
-        points_in_paint: homeStats.pointsInPaint, // 禁區得分 PITP
-        points_off_turnovers: homeStats.pointsOffTurnovers, // 因對方失誤得分 POT
-        biggest_lead: homeStats.biggestLead // 最多領先分數 BL
+        rebounds: homeStats.rebs,
+        turnovers: homeStats.turnovers,
+        assists: homeStats.assists,
+        blocks: homeStats.blocksAgainst,
+        steals: homeStats.steals,
+        fast_break_points: homeStats.fastBreakPoints,
+        points_in_paint: homeStats.pointsInPaint,
+        points_off_turnovers: homeStats.pointsOffTurnovers,
+        biggest_lead: homeStats.biggestLead
       };
-      for (const key in awayData) await set2realtime(`${path}/Summary/info/away/Total/${key}`, awayData[key]);
-      for (const key in homeData) await set2realtime(`${path}/Summary/info/home/Total/${key}`, homeData[key]);
-      console.log(`更新 NBA: ${gameId} 文字直播`);
+      for (const key in awayData) await set2realtime(`${path}/info/away/Total/${key}`, awayData[key]);
+      for (const key in homeData) await set2realtime(`${path}/info/home/Total/${key}`, homeData[key]);
+      // console.log(`更新 NBA: ${gameId} 文字直播`);
     }
     return Promise.resolve();
   } catch (err) {
