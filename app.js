2518afe9c6654a9e24b4ab4cc1856af1fd37c536
d0177381@gmail.com <>
2020-09-16 18:20:32 +0800

    [Add] scheduler

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const schedule = require('node-schedule-tz');
 const NBA_match = require('./src/invoke/basketball/NBA_match');
 const NBA_livescore = require('./src/invoke/basketball/NBA_livescore');
 const MLB_match = require('./src/invoke/baseball/MLB_match');
+const MLB_status = require('./src/invoke/baseball/MLB_status');
 const { zone_tw } = process.env;
 const { taipeiDate } = require('./src/helpers/momentUtil');
 // const connection = require('./src/helpers/connection');
@@ -14,17 +15,15 @@ const app = express();
 
 app.use(Have.haven());
 
-schedule.scheduleJob('0 0 11 * * *', async function() {
-  // 取得 Token
+schedule.scheduleJob('取得 Token', '0 0 11 * * *', zone_tw, async function() {
   // HW.getToken();
 });
 
-schedule.scheduleJob('*/10 * * * * *', async function() {
-  // 取得盤口
+schedule.scheduleJob('取得盤口', '*/10 * * * * *', zone_tw, async function() {
   // await HW.getHandicap();
 });
 
-schedule.scheduleJob('*/3 * * * * *', async function() {
+schedule.scheduleJob('文字直播', '*/3 * * * * *', zone_tw, async function() {
   try {
     // console.log(`NBA_livescore was supposed to run at ${taipeiDate(new Date())}`);
     await NBA_livescore();
@@ -34,11 +33,19 @@ schedule.scheduleJob('*/3 * * * * *', async function() {
 });
 
 // If a timezone is specified, a job name must be specified as well as the first parameter.
-schedule.scheduleJob('Match information', '0 */4 * * *', zone_tw, async function() {
+schedule.scheduleJob('賽程', '0 */4 * * *', zone_tw, async function() {
   try {
     console.log(`Match run at ${taipeiDate(new Date())}`);
     await NBA_match();
     await MLB_match();
+  } catch (err) {
+    console.log(err);
+  }
+});
+schedule.scheduleJob('監聽賽事狀態', '0 */1 * * * *', zone_tw, async function() {
+  try {
+    // console.log(`Status run at ${taipeiDate(new Date())}`);
+    await MLB_status();
   } catch (err) {
     console.log(err);
   }
