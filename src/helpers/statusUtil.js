fbe9a9fdab19042819eaca1c3c547bf818f02f86
d0177381@gmail.com <>
2020-09-17 13:48:53 +0800

    [Tune] should check codedGameState, abstractGameCode in MLB_statusMapping function

--- src/helpers/statusUtil.js
+++ src/helpers/statusUtil.js
@@ -1,18 +1,12 @@
 const MATCH_STATUS = { SCHEDULED: 2, INPLAY: 1, END: 0, ABNORMAL: -1, VALID: 1, POSTPONED: -2, CANCELLED: -3, TBD: -10 };
 
-function MLB_statusMapping(status) {
-  switch (status) {
-    case 'S':
-      return MATCH_STATUS.SCHEDULED;
-    case 'I':
-      return MATCH_STATUS.INPLAY;
-    case 'F':
-      return MATCH_STATUS.END;
-    case 'D':
-      return MATCH_STATUS.POSTPONED;
-    default:
-      throw new Error(`Unknown status: ${status} in MLB`);
-  }
+function MLB_statusMapping(matchId, status) {
+  const { detailedStatus, codedGameState, abstractGameCode } = status;
+  if (abstractGameCode === 'P') return MATCH_STATUS.SCHEDULED;
+  else if (abstractGameCode === 'L') return MATCH_STATUS.INPLAY;
+  else if ((abstractGameCode === 'F' && codedGameState === 'F') || (abstractGameCode === 'F' && codedGameState === 'O')) return MATCH_STATUS.END;
+  else if (abstractGameCode === 'F' && codedGameState === 'D') return MATCH_STATUS.POSTPONED;
+  throw new Error(`MLB 比賽編號: ${matchId} 的未知狀態: ${abstractGameCode}(${detailedStatus})`);
 }
 
 module.exports = {
