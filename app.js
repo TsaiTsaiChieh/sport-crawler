d5169f7f6871dc548fdec114ee69a47f9d5f08c1
d0177381@gmail.com <>
2020-09-18 14:38:31 +0800

    [Update] general setting

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const Have = require('domain-haven');
 const schedule = require('node-schedule-tz');
-const HW = require('./src/handicap/HW/HW_handicap');
-// const NBA_match = require('./src/invoke/basketball/NBA_match');
-// const NBA_livescore = require('./src/invoke/basketball/NBA_livescore');
-// const MLB_match = require('./src/invoke/baseball/MLB_match');
-// const MLB_status = require('./src/invoke/baseball/MLB_status');
-// const KBO_match = require('./src/crawler/baseball/KBO_match');
-// const KBO_status = require('./src/crawler/baseball/KBO_status');
+// const HW = require('./src/handicap/HW/HW_handicap');
+const NBA_match = require('./src/invoke/basketball/NBA_match');
+const NBA_livescore = require('./src/invoke/basketball/NBA_livescore');
+const MLB_match = require('./src/invoke/baseball/MLB_match');
+const MLB_status = require('./src/invoke/baseball/MLB_status');
+const KBO_match = require('./src/crawler/baseball/KBO_match');
+const KBO_status = require('./src/crawler/baseball/KBO_status');
 const { zone_tw } = process.env;
 const { taipeiDate } = require('./src/helpers/momentUtil');
 // const connection = require('./src/helpers/connection');
@@ -25,13 +25,13 @@ app.use(Have.haven());
 schedule.scheduleJob('*/10 * * * * *', async function() {
   // 取得盤口
   console.log(`Handicap run at ${taipeiDate(new Date())}`);
-  await HW.getHandicap();
+  // await HW.getHandicap();
 });
 
 schedule.scheduleJob('文字直播', '*/3 * * * * *', zone_tw, async function() {
   try {
     // console.log(`NBA_livescore was supposed to run at ${taipeiDate(new Date())}`);
-    // await NBA_livescore();
+    await NBA_livescore();
   } catch (err) {
     console.log(err);
   }
@@ -41,9 +41,9 @@ schedule.scheduleJob('文字直播', '*/3 * * * * *', zone_tw, async function() 
 schedule.scheduleJob('賽程', '0 */4 * * *', zone_tw, async function() {
   try {
     // console.log(`Match run at ${taipeiDate(new Date())}`);
-    // await NBA_match();
-    // await MLB_match();
-    // await KBO_match();
+    await NBA_match();
+    await MLB_match();
+    await KBO_match();
   } catch (err) {
     console.log(err);
   }
@@ -51,9 +51,9 @@ schedule.scheduleJob('賽程', '0 */4 * * *', zone_tw, async function() {
 schedule.scheduleJob('監聽賽事狀態', '0 */1 * * * *', zone_tw, async function() {
   try {
     // console.log(`Status run at ${taipeiDate(new Date())}`);
-    // await NBA_match();
-    // await MLB_status();
-    // await KBO_status();
+    await NBA_match();
+    await MLB_status();
+    await KBO_status();
   } catch (err) {
     console.log(err);
   }
