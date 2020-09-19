4fe7e4410ba5b9b42bfbd582c114b226ccc9dc7f
d0177381@gmail.com <>
2020-09-19 23:43:12 +0800

    [Refactor] NBA match & livescore code

--- src/invoke/basketball/NBA_match.js
+++ src/invoke/basketball/NBA_match.js
@@ -1,30 +1,21 @@
-const momentUtil = require('../../helpers/momentUtil');
+const configs = require('../../configs/league/NBA_configs');
+const { timestamp2date } = require('../../helpers/momentUtil');
 const { getData } = require('../../helpers/invokeUtil');
 const ServerErrors = require('../../helpers/ServerErrors');
 const { NBA_teamName2id } = require('../../helpers/teamsMapping');
-const mysql = require('../../helpers/mysqlUtil');
-const { MATCH_STATUS, league2Sport, leagueCodebook } = require('../../helpers/leaguesUtil');
-
-const configs = {
-  league: 'NBA',
-  API: 'https://tw.global.nba.com/stats2/season/schedule.json',
-  gameDate: '',
-  countryCode: process.env.countryCode,
-  days: 1,
-  locale: process.env.locale,
-  tz: `%2B${process.env.GMT}`
-};
+const { updateMatchChunk2MySQL } = require('../../helpers/databaseEngine');
 
 async function main() {
   try {
-    let { API, gameDate, countryCode, days, locale, tz } = configs;
-    gameDate = momentUtil.timestamp2date(Date.now(), { op: 'add', value: 1, unit: 'days', format: 'YYYY-MM-DD' });
-    const URL = `${API}?gameDate=${gameDate}&countryCode=${countryCode}&days=${days}&locale=${locale}&tz=${tz}`;
+    let { matchAPI, gameDate, countryCode, days, locale, tz } = configs;
+    gameDate = timestamp2date(Date.now(), { op: 'add', value: 1, unit: 'days', format: 'YYYY-MM-DD' });
+    const URL = `${matchAPI}?gameDate=${gameDate}&countryCode=${countryCode}&days=${days}&locale=${locale}&tz=${tz}`;
     const data = await getData(URL);
     const matchChunk = await repackageMatches(data);
-    await update2MySQL(matchChunk);
+    await updateMatchChunk2MySQL(matchChunk, configs);
+    return Promise.resolve();
   } catch (err) {
-    return err;
+    return Promise.reject(err.stack);
   }
 }
 
@@ -46,33 +37,6 @@ async function repackageMatches(matchData) {
     return Promise.resolve(data);
   } catch (err) {
     return Promise.reject(new ServerErrors.RepackageError(err.stack));
-  }
-}
-
-async function update2MySQL(data) {
-  try {
-    const leagueId = leagueCodebook(configs.league).id;
-    const sportId = league2Sport(configs.league).sport_id;
-    const oriLeagueId = leagueCodebook(configs.league).ori_league_id;
-    data.map(async function(ele) {
-      await mysql.Match.upsert({
-        bets_id: ele.matchId,
-        league_id: leagueId,
-        sport_id: sportId,
-        home_id: ele.homeId,
-        away_id: ele.awayId,
-        scheduled: ele.scheduled / 1000,
-        scheduled_tw: ele.scheduled,
-        flag_prematch: MATCH_STATUS.VALID,
-        status: MATCH_STATUS.SCHEDULED,
-        ori_league_id: oriLeagueId,
-        ori_sport_id: sportId
-      });
-      console.log(`更新 NBA: ${ele.matchId} - ${ele.awayAliasCh}(${ele.awayId}) vs ${ele.homeAliasCh}(${ele.homeId}) 成功`);
-    });
-    return Promise.resolve();
-  } catch (err) {
-    return Promise.reject(new ServerErrors.MySQLError(err.stack));
   }
 }
 
