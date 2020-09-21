8d1ebe4f45b4a78413606c6166a2df7aa5272dad
d0177381@gmail.com <>
2020-09-21 11:20:16 +0800

    [Fix] status field is null problem

--- src/invoke/basketball/NBA_match.js
+++ src/invoke/basketball/NBA_match.js
@@ -4,11 +4,12 @@ const { getData } = require('../../helpers/invokeUtil');
 const ServerErrors = require('../../helpers/ServerErrors');
 const { NBA_teamName2id } = require('../../helpers/teamsMapping');
 const { updateMatchChunk2MySQL } = require('../../helpers/databaseEngine');
+const { MATCH_STATUS } = require('../../helpers/leaguesUtil');
 
 async function main() {
   try {
     let { matchAPI, gameDate, countryCode, days, locale, tz } = configs;
-    gameDate = timestamp2date(Date.now(), { op: 'add', value: 1, unit: 'days', format: 'YYYY-MM-DD' });
+    gameDate = timestamp2date(Date.now(), { op: 'add', value: 0, unit: 'days', format: 'YYYY-MM-DD' });
     const URL = `${matchAPI}?gameDate=${gameDate}&countryCode=${countryCode}&days=${days}&locale=${locale}&tz=${tz}`;
     const data = await getData(URL);
     const matchChunk = await repackageMatches(data);
@@ -26,6 +27,7 @@ async function repackageMatches(matchData) {
       data.push({
         matchId: ele.profile.gameId,
         scheduled: parseInt(ele.profile.utcMillis),
+        status: MATCH_STATUS.SCHEDULED,
         homeId: NBA_teamName2id(ele.homeTeam.profile.abbr),
         homeAlias: ele.homeTeam.profile.abbr,
         homeAliasCh: ele.homeTeam.profile.displayAbbr,
