08dcc29cf8621d1313be07361f4be712d480090b
d0177381@gmail.com <>
2020-09-22 14:50:53 +0800

    [Fix] NBA gameDate and games undefined problem in repackageMatches function

--- src/invoke/basketball/NBA_match.js
+++ src/invoke/basketball/NBA_match.js
@@ -9,7 +9,7 @@ const { MATCH_STATUS } = require('../../helpers/leaguesUtil');
 async function main() {
   try {
     let { matchAPI, gameDate, countryCode, days, locale, tz } = configs;
-    gameDate = timestamp2date(Date.now(), { op: 'add', value: 0, unit: 'days', format: 'YYYY-MM-DD' });
+    gameDate = timestamp2date(Date.now(), { op: 'add', value: 1, unit: 'days', format: 'YYYY-MM-DD' });
     const URL = `${matchAPI}?gameDate=${gameDate}&countryCode=${countryCode}&days=${days}&locale=${locale}&tz=${tz}`;
     const data = await getData(URL);
     const matchChunk = await repackageMatches(data);
@@ -23,19 +23,21 @@ async function main() {
 async function repackageMatches(matchData) {
   try {
     const data = [];
-    matchData.payload.dates[0].games.map(function(ele) {
-      data.push({
-        matchId: ele.profile.gameId,
-        scheduled: parseInt(ele.profile.utcMillis),
-        status: MATCH_STATUS.SCHEDULED,
-        homeId: NBA_teamName2id(ele.homeTeam.profile.abbr),
-        homeAlias: ele.homeTeam.profile.abbr,
-        homeAliasCh: ele.homeTeam.profile.displayAbbr,
-        awayId: NBA_teamName2id(ele.awayTeam.profile.abbr),
-        awayAlias: ele.awayTeam.profile.abbr,
-        awayAliasCh: ele.awayTeam.profile.displayAbbr
+    if (matchData.payload.dates.length) {
+      matchData.payload.dates[0].games.map(function(ele) {
+        data.push({
+          matchId: ele.profile.gameId,
+          scheduled: parseInt(ele.profile.utcMillis),
+          status: MATCH_STATUS.SCHEDULED,
+          homeId: NBA_teamName2id(ele.homeTeam.profile.abbr),
+          homeAlias: ele.homeTeam.profile.abbr,
+          homeAliasCh: ele.homeTeam.profile.displayAbbr,
+          awayId: NBA_teamName2id(ele.awayTeam.profile.abbr),
+          awayAlias: ele.awayTeam.profile.abbr,
+          awayAliasCh: ele.awayTeam.profile.displayAbbr
+        });
       });
-    });
+    }
     return Promise.resolve(data);
   } catch (err) {
     return Promise.reject(new ServerErrors.RepackageError(err.stack));
