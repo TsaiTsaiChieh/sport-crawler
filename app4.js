68d0c94cab0be8bd052203390670ce07f2b51314
d0177381@gmail.com <>
2020-09-23 11:14:39 +0800

    [Add] envValues

--- app4.js
+++ app4.js
@@ -4,13 +4,14 @@ const Have = require('domain-haven');
 const schedule = require('node-schedule-tz');
 const { zone_tw } = process.env;
 const { APP4_PORT } = process.env;
+const envValues = require('./src/configs/envValues');
 
 const app = express();
 app.use(Have.haven());
 
 schedule.scheduleJob('文字直播', '*/3 * * * * *', zone_tw, async function() {
   try {
-    console.log(process.env.status);
+    console.log(envValues.cert);
     return;
   } catch (err) {
     console.log(err);
--- src/configs/envValues.js
+++ src/configs/envValues.js
@@ -1,12 +1,20 @@
-exports.firebaseConfig = {
-  apiKey: process.env.firebaseApiKey,
-  authDomain: process.env.firebaseAuthDomain,
-  databaseURL: process.env.firebaseDatabaseURL,
-  projectId: process.env.GCLOUD_PROJECT,
-  storageBucket: process.env.firebaseStorageBucket,
-  messagingSenderId: process.env.firebaseMessagingSenderId,
-  appId: process.env.firebaseAppId,
-  measurementId: process.env.firebaseMeasurementId
+// The NODE_ENV of VM is [launching]，localhost is undefined
+const isEmulator = process.env.NODE_ENV === 'test';
+const { env } = process;
+const firebaseConfig = {
+  apiKey: isEmulator ? env['test-firebaseApiKey'] : env['product-firebaseApiKey'],
+  authDomain: isEmulator ? env['test-firebaseAuthDomain'] : env['product-firebaseAuthDomain'],
+  databaseURL: isEmulator ? env['test-firebaseDatabaseURL'] : env['product-firebaseDatabaseURL'],
+  projectId: isEmulator ? env['test-GCLOUD_PROJECT'] : env['product-GCLOUD_PROJECT'],
+  storageBucket: isEmulator ? env['test-firebaseStorageBucket'] : env['product-firebaseStorageBucket'],
+  messagingSenderId: isEmulator ? env['test-firebaseMessagingSenderId'] : env['product-firebaseMessagingSenderId'],
+  appId: isEmulator ? env['test-firebaseAppId'] : env['product-firebaseAppId'],
+  measurementId: isEmulator ? env['test-firebaseMeasurementId'] : env['product-firebaseMeasurementId']
 };
-
-exports.betsToken = '46719-gZEnjYySo0cLKx';
+const betsToken = '46719-gZEnjYySo0cLKx';
+const cert = isEmulator ? env['test-certPath'] : env['product-certPath'];
+module.exports = {
+  firebaseConfig,
+  betsToken,
+  cert
+};
