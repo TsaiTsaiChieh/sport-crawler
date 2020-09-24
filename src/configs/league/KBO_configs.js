680e051504ebc5c3481f62904b4c0fd4b85c9b08
d0177381@gmail.com <>
2020-09-24 17:48:36 +0800

    [Add] livescoreURL, baseURL setting in KBO

--- src/configs/league/KBO_configs.js
+++ src/configs/league/KBO_configs.js
@@ -1,12 +1,30 @@
 const league = 'KBO';
 const { league2Sport, leagueCodebook } = require('../../helpers/leaguesUtil');
+
+/**
+ * * matchURL example:
+ * * https://mykbostats.com/api/v2/games/game-day-block/2020-09-17
+ * * livescoreURL example:
+ * * https://m.sports.naver.com/ajax/baseball/gamecenter/kbo/todayGames.nhn?gameId=20200923KTLT02020
+ * * baseURL example:
+ * * https://www.koreabaseball.com/ws/Main.asmx/GetKboGameList?leId=1&srId=0&date=20200922
+*/
+
 const configs = {
   league,
+  sport: league2Sport(league).sport,
   matchURL: 'https://mykbostats.com/api/v2/games/game-day-block/',
+  livescoreURL: 'https://m.sports.naver.com/ajax/baseball/gamecenter/kbo/todayGames.nhn',
+  baseURL: 'https://www.koreabaseball.com/ws/Main.asmx/GetKboGameList',
   date: '',
+  leId: 1,
+  srId: 0,
+  gameId: '',
+  nullThreshold: 200,
   league_id: leagueCodebook(league).id,
   sport_id: league2Sport(league).sport_id,
-  ori_league_id: leagueCodebook(league).ori_league_id
+  ori_league_id: leagueCodebook(league).ori_league_id,
+  KoreaZone: 'Asia/Seoul'
 };
 
 module.exports = configs;
