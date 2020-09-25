674de7d1d6fa37c81bc4b4ff08c54967ae2445d0
d0177381@gmail.com <>
2020-09-26 02:01:08 +0800

    [Add] livescore ULR setting

--- src/configs/league/NPB_configs.js
+++ src/configs/league/NPB_configs.js
@@ -1,11 +1,23 @@
 const { league2Sport, leagueCodebook } = require('../../helpers/leaguesUtil');
+
+/**
+ * * livescoreURL example: (獵分網)
+ * * https://score.com.tw/dbjson.php?date=2020-09-25&sporttype=bb&union=bb_2
+*/
+
 const league = 'NPB';
 const configs = {
   league,
+  livescoreURL: 'https://score.com.tw/dbjson.php',
+  // livescoreURL
+  date: '',
+  sporttype: 'bb',
+  union: 'bb_2', // 日棒,
   sport: league2Sport(league).sport,
   league_id: leagueCodebook(league).id,
   sport_id: league2Sport(league).sport_id,
-  ori_league_id: leagueCodebook(league).ori_league_id
+  ori_league_id: leagueCodebook(league).ori_league_id,
+  japanZone: 'Asia/Tokyo'
 };
 
 module.exports = configs;
