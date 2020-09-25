0ed83248e176f886fd9336e10a3a8388c08042bc
d0177381@gmail.com <>
2020-09-25 18:19:40 +0800

    [Finish] CPBL status listener

--- src/crawler/baseball/CPBL_status.js
+++ src/crawler/baseball/CPBL_status.js
@@ -1,60 +1,75 @@
-// const configs = require('../../configs/league/CPBL_configs');
-// const momentUtil = require('../../helpers/momentUtil');
-// const { getScheduledAndInplayMatchesFromMySQL } = require('../../helpers/databaseEngine');
-// const { crawler } = require('../../helpers/invokeUtil');
+const configs = require('../../configs/league/CPBL_configs');
+const momentUtil = require('../../helpers/momentUtil');
+const { getScheduledAndInplayMatchesFromMySQL } = require('../../helpers/databaseEngine');
+const moment = require('moment');
+require('moment-timezone');
+const { getData } = require('../../helpers/invokeUtil');
+const ServerErrors = require('../../helpers/ServerErrors');
+const { CPBL_teamName2id } = require('../../helpers/teamsMapping');
+const { MATCH_STATUS, CPBL_statusMapping } = require('../../helpers/statusUtil');
+const mysql = require('../../helpers/mysqlUtil');
 
-// async function main() {
-//   try {
-//     const { league_id } = configs;
-//     const nowUnix = Math.floor(Date.now() / 1000);
-//     // const matchData = await getScheduledAndInplayMatchesFromMySQL(nowUnix, league_id);
-//     const matchData = [{
-//       matchId: '2020092511235G211',
-//       status: 2,
-//       scheduled: 1601030100,
-//       scheduled_tw: '2020-09-25T10:35:00.000Z',
-//       homeId: '329121',
-//       homeAlias: 'Rakuten Monkeys',
-//       awayId: '230422',
-//       awayAlias: 'CTBC Brothers'
-//     },
-//     {
-//       matchId: '2020092511235G212',
-//       status: 2,
-//       scheduled: 1601030100,
-//       scheduled_tw: '2020-09-25T10:35:00.000Z',
-//       homeId: '224095',
-//       homeAlias: 'Uni-President Lions',
-//       awayId: '224094',
-//       awayAlias: 'Fubon Guardians'
-//     }];
-//     if (matchData.length) await crawlerURL(matchData);
-//   } catch (err) {
-//     return Promise.reject(err.stack);
-//   }
-// }
+async function main() {
+  try {
+    const { league_id } = configs;
+    const nowUnix = Math.floor(Date.now() / 1000);
+    const matchData = await getScheduledAndInplayMatchesFromMySQL(nowUnix, league_id);
+    if (matchData.length) await invokeAPI(matchData);
+  } catch (err) {
+    return Promise.reject(err.stack);
+  }
+};
 
-// async function crawlerURL(matchData) {
-//   try {
-//     const { livescoreURL, lang, ball } = configs;
-//     const URL = `${livescoreURL}?lang=${lang}&ball=${ball}`;
-//     const $ = await crawler(URL);
-//     await repackageStatusData($);
-//     // console.log(data);
-//     return Promise.resolve();
-//   } catch (err) {
-//     return Promise.reject(err.stack);
-//   }
-// }
+async function invokeAPI(matchData) {
+  try {
+    let { livescoreURL, date, sporttype, union } = configs;
+    date = momentUtil.timestamp2date(Date.now(), { format: 'YYYY-MM-DD' });
+    const URL = `${livescoreURL}?date=${date}&sporttype=${sporttype}&union=${union}`;
+    const data = await getData(URL);
+    const matchChunk = await repackageMatchData(date, data, matchData);
+    await updateStatusOrScore2MySQL(matchChunk);
+    return Promise.resolve();
+  } catch (err) {
+    return Promise.reject(err.stack);
+  }
+}
 
-// async function repackageStatusData($) {
-//   try {
-//     $('tbody').each(function(i) {
-//       console.log($(this).text);
-//     });
-//   } catch (err) {
-//     return Promise.reject(err.stack);
-//   }
-// }
+function repackageMatchData(date, gameData, matchData) {
+  try {
+    const data = [];
+    gameData.data.map(function(game) {
+      const homeId = CPBL_teamName2id(game.home);
+      const awayId = CPBL_teamName2id(game.away);
+      const time = game.runtime;
+      const scheduled = moment.tz(`${date} ${time}`, 'YYYY-MM-DD hh:mm', configs.taiwanZone).unix();
+      matchData.map(function(match) {
+        if (homeId === match.homeId && awayId === match.awayId && scheduled === match.scheduled) {
+          data.push({
+            matchId: match.matchId,
+            status: CPBL_statusMapping(game.status),
+            homeScore: game.score_a,
+            awayScore: game.score_b
+          });
+        }
+      });
+    });
+    return Promise.resolve(data);
+  } catch (err) {
+    return Promise.reject(new ServerErrors.RepackageError(err.stack));
+  }
+}
 
-// module.exports = main;
+async function updateStatusOrScore2MySQL(matchChunk) {
+  try {
+    const { INPLAY, END } = MATCH_STATUS;
+    matchChunk.map(async function(match) {
+      if (match.status === END) await mysql.Match.update({ status: match.status, home_points: match.homeScore, away_points: match.awayScore }, { where: { bets_id: match.matchId } });
+      if (match.status === INPLAY) await mysql.Match.update({ status: match.status }, { where: { bets_id: match.matchId } });
+    });
+    return Promise.resolve();
+  } catch (err) {
+    return Promise.reject(new ServerErrors.MySQLError(err.stack));
+  }
+}
+
+module.exports = main;
