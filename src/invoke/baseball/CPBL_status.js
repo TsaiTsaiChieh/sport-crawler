e80920931aead2e0e2453057eccc2c519311d08d
d0177381@gmail.com <>
2020-10-12 11:52:52 +0800

    [Add] CPBL status listner in realtime DB

--- src/invoke/baseball/CPBL_status.js
+++ src/invoke/baseball/CPBL_status.js
@@ -8,6 +8,7 @@ const ServerErrors = require('../../helpers/ServerErrors');
 const { CPBL_teamIncludes2id } = require('../../helpers/teamsMapping');
 const { MATCH_STATUS, CPBL_statusMapping } = require('../../helpers/statusUtil');
 const mysql = require('../../helpers/mysqlUtil');
+const { set2realtime } = require('../../helpers/firebaseUtil');
 
 async function main() {
   try {
@@ -80,19 +81,25 @@ function checkMatchStatus(game, match, statusFunction) {
 async function updateStatusOrScore2MySQL(matchChunk) {
   try {
     const { INPLAY, END, POSTPONED } = MATCH_STATUS;
+    const { league, sport } = configs;
+    const path = `${sport}/${league}`;
     matchChunk.map(async function(match) {
       const now = momentUtil.taipeiDate(new Date());
+      const { matchId } = match;
       if (match.status === END) {
-        await mysql.Match.update({ status: match.status, home_points: match.homeScore, away_points: match.awayScore }, { where: { bets_id: match.matchId } });
+        await set2realtime(`${path}/${matchId}/Summary/status`, { status: END });
+        await mysql.Match.update({ status: match.status, home_points: match.homeScore, away_points: match.awayScore }, { where: { bets_id: matchId } });
         console.log(`CPBL - ${match.matchId} 完賽 at ${now}`);
       }
       if (match.oriStatus !== INPLAY && match.status === INPLAY) {
-        await mysql.Match.update({ status: match.status, scheduled: match.scheduled, scheduled_tw: match.scheduled * 1000, sr_id: match.gameId }, { where: { bets_id: match.matchId } });
-        console.log(`CPBL - ${match.matchId} 開賽 at ${now}`);
+        await set2realtime(`${path}/${matchId}/Summary/status`, { status: INPLAY });
+        await mysql.Match.update({ status: match.status, scheduled: match.scheduled, scheduled_tw: match.scheduled * 1000, sr_id: match.gameId }, { where: { bets_id: matchId } });
+        console.log(`CPBL - ${matchId} 開賽 at ${now}`);
       }
       if (match.status === POSTPONED) {
-        await mysql.Match.update({ status: match.status }, { where: { bets_id: match.matchId } });
-        console.log(`CPBL - ${match.matchId} 延賽 at ${now}`);
+        await set2realtime(`${path}/${matchId}/Summary/status`, { status: POSTPONED });
+        await mysql.Match.update({ status: match.status }, { where: { bets_id: matchId } });
+        console.log(`CPBL - ${matchId} 延賽 at ${now}`);
       }
     });
     return Promise.resolve();
