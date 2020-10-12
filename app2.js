2ee3a962b519e9b3c4edc7fcb7cf367e598e1534
d0177381@gmail.com <>
2020-10-12 14:43:55 +0800

    [Remove] NBA scheduler

--- app2.js
+++ app2.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const Have = require('domain-haven');
 const schedule = require('node-schedule-tz');
-const NBA = {
-  match: require('./src/invoke/basketball/NBA_match'),
-  livescore: require('./src/invoke/basketball/NBA_livescore')
-};
+// const NBA = {
+//   match: require('./src/invoke/basketball/NBA_match'),
+//   livescore: require('./src/invoke/basketball/NBA_livescore')
+// };
 
 const KBO = {
   match: require('./src/invoke/baseball/KBO_match'),
@@ -19,25 +19,25 @@ const { APP2_PORT } = process.env;
 const app = express();
 app.use(Have.haven());
 
-schedule.scheduleJob('NBA 賽程', '5 */1 * * *', zone_tw, async function() {
-  try {
-    await NBA.match();
-    return;
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
-});
+// schedule.scheduleJob('NBA 賽程', '5 */1 * * *', zone_tw, async function() {
+//   try {
+//     await NBA.match();
+//     return;
+//   } catch (err) {
+//     console.log(err);
+//     return err;
+//   }
+// });
 
-schedule.scheduleJob('NBA 文字直播', '*/5 * * * * *', zone_tw, async function() {
-  try {
-    await NBA.livescore();
-    return;
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
-});
+// schedule.scheduleJob('NBA 文字直播', '*/5 * * * * *', zone_tw, async function() {
+//   try {
+//     await NBA.livescore();
+//     return;
+//   } catch (err) {
+//     console.log(err);
+//     return err;
+//   }
+// });
 
 schedule.scheduleJob('KBO 文字直播', '*/5 * * * * *', zone_tw, async function() {
   try {
@@ -70,5 +70,6 @@ schedule.scheduleJob('KBO 監聽賽事狀態', '0 */1 * * * *', zone_tw, async f
 });
 
 app.listen(APP2_PORT, function() {
-  console.log(`NBA & KBO crawler on port: ${APP2_PORT}`);
+  // console.log(`NBA & KBO crawler on port: ${APP2_PORT}`);
+  console.log(`KBO crawler on port: ${APP2_PORT}`);
 });
